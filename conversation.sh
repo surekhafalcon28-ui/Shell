b161acd22689b563fa8b96981f1e14c843fc0350
Surekha <surekha@Surekhas-MacBook-Air.local>
2026-06-25 13:32:31 +0300

    added username_passowrd

--- conversation.sh
+++ conversation.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
+PERSON1="$1"
+PERSON2="$2"
 
-echo "Hello! I am your AI assistant. How can I help you today?"
+echo "Hello! I am your $PERSON1 assistant. How can I help you today?"
 echo "You can ask me questions, request information, or seek assistance with various tasks."
 echo "Feel free to type your query below:"
-echo "Type 'exit' to end the conversation."
+echo "$PERSON2 Type 'exit' to end the conversation."
---
+++ varriables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Please enter your username:"
+read USER_NAME
+echo "Username: $USER_NAME"
+echo "Please enter your password:"
+read -s PASSWORD
+echo "Password: $PASSWORD"
